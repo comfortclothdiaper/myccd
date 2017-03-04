554f6a0a6c4f00e720b18b98b43aff525231214b
Asmazabin Qureshi <Asmazabin.Qureshi@gatewaygroup.com>
2017-03-04 18:22:28 +0530

    plz

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
 ﻿var app = angular.module('myApp', [])
-.controller('myctrl', function ($scope, $http) {
-	//$scope.abc = {};
-	//$scope.profileID = "";
-	$scope.toastMessage = "";
-	//alert('Done');
+.controller('myctrl', function ($scope, $http) {	
+	$scope.toastMessage = "";	
 	$scope.submit = function () {
 		var saveDetail = {
 			'name': $scope.name,
 			'email': $scope.email,
 			'contactNum': $scope.contactNumber,
 			'message': $scope.message			
-		};
-		// console.log(saveDetail);
-		$http.post("http://localhost:8000/api/saveRetailerDetails" , saveDetail).success(function (response) {
-			// console.log(response);
+		};	
+		$http.post("http://localhost:8000/api/saveRetailerDetails" , saveDetail).success(function (response) {		
 			if (response.status===201) { 
 				$scope.toastMessage = "Details successfully saved,We will get back to you soon.";
-				angular.element('#successMsg').stop().slideDown(1000).delay(1500).fadeOut(1000);
-				//$scope.contactForm.$setPristine();				
+				angular.element('#successMsg').stop().slideDown(1000).delay(1500).fadeOut(1000);					
 				contactForm.reset();
 					$scope.contactForm.$pristine = true;
 				$scope.contactForm.$submitted = false;
